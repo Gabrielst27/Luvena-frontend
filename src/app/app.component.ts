cea6629d31b2e6cee52074b8677e87bd7ac35bc9
Gabriel Torres <gabrielst328@gmail.com>
2024-10-24 23:18:32 -0300

    refactor: refatoracao page register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'luvena.frontend';
+
+  constructor(private router: Router){}
+
+  hide(): boolean {
+    const currentRoute = this.router.url;
+    return currentRoute === '/register' || currentRoute === '/login';
+  }
 }
